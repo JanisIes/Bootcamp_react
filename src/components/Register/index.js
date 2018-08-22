5c97e67ad39bb07bc81c08acdee386a0cc66a161
Jānis Iesalnieks <iesalnieks.juniors@gmail.com>
2018-08-22 16:56:55 +0300

    Login + register + connecting with server and db

---
+++ src/components/Register/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: ""
+  };
+
+  onChange = event =>
+    this.setState({ [event.target.name]: event.target.value });
+  handleRegister = e => {
+    e.preventDefault();
+    const { email, username, password } = this.state;
+    this.props.register(email, username, password).then(() => {
+      if (this.props.registerReducer.isRegistered) {
+        this.props.history.push("/login");
+      }
+      console.log(this.state);
+    });
+  };
+
+  render() {
+    return (
+      <section className="container">
+        <form className="w-50 mx-auto mt-5">
+          <div className="form-group">
+            <label for="exampleInputUsername1">Username</label>
+            <input
+              type="username"
+              name="username"
+              className="form-control"
+              id="exampleInputUsername1"
+              placeholder="Username"
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              onChange={this.onChange}
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={this.handleRegister}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default Register;
